adf996c7934af8df7965aa82bfb1aab3b5242292
Mia Johansson <miajohansson@Mias-MBP.attlocal.net>
2016-12-27 11:17:50 -0800

    Added Redcarpet & Pygments.rb for Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def markdown(content)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html:true,
+    autolink: true,
+    no_intra_empasis: true,
+    disable_indented_code_blocks: true,
+    fenced_code_blocks: true,
+    strikethough: true,
+    superscirpt: true)
+
+    Redcarpet::Markdown.new(renderer).render(content).html_safe
+  end
 end
