225094ebc85d36519a1dfd4dab29c4fb998d468e
Mia Johansson <miajohansson@Mias-MacBook-Pro.local>
2017-01-04 20:26:28 -0800

    Html code in posts

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'friendly_id', '~> 5.1'
 gem 'will_paginate', '~> 3.1', '>= 3.1.5'
 gem 'mail_form', '~> 1.5', '>= 1.5.1'
 gem 'devise', '~> 4.2'
+gem 'kramdown', '~> 1.9'
 
 group :development, :test do
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def markdown(content)
-    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html:true,
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true,
     autolink: true,
     no_intra_empasis: true,
     disable_indented_code_blocks: true,
