a332f75054ec4c969db72f201b1388326a029f04
Mia Johansson <miajohansson@Mias-MacBook-Pro.local>
2017-01-05 08:56:45 -0800

    Changed article limit to 8

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.all.limit(4).order("created_at desc")
+    @posts = Post.all.limit(8).order("created_at desc")
     @projects = Project.all
   end
 end
