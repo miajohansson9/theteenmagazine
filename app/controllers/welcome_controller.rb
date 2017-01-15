72cad869ca8740a751843cb489488a4450247347
Mia Johansson <miajohansson@Mias-MBP.attlocal.net>
2017-01-15 14:01:23 -0800

    Blog update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.all.limit(8).order("created_at desc")
+    @posts = Post.all.order("created_at desc")
     @projects = Project.all
   end
 end
