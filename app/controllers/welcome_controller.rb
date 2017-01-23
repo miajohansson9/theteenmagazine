5a5ac1d6ad3dc2ede56c107b82f8d30d973cf6bb
Mia Johansson <miajohansson@Mias-MBP.attlocal.net>
2017-01-22 20:44:56 -0800

    Redesigned home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @posts = Post.all.order("created_at desc")
     @projects = Project.all
+    @posts_featured = Post.all.order("created_at desc").first
   end
 end
