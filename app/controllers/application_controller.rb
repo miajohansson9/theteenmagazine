60377cad37a8f68b245aa69fed3d10d6610e1195
Mia Johansson <miajohansson@Mias-MBP.attlocal.net>
2017-01-28 13:59:43 -0800

    Fixed! (I think)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :load_projects
+
+  def load_projects
+    @projects = Project.all
+  end
   protect_from_forgery with: :exception
 end
