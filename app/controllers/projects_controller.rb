41483c09dfa7e4b7a72bb7e358c452ecfb19fe92
Mia Johansson <miajohansson@Mias-MBP.attlocal.net>
2017-02-04 22:03:58 -0800

    Redid landing page :)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,7 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
     @projects = Project.all.order("created_at desc")
+    set_meta_tags title: 'The Teen Magazine | Projects'
   end
 
   def new
