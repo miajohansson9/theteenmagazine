e8a80b0c63ca218b64a9d1f01b06c50d59d2b6ec
Mia Johansson <miajohansson@Mias-MBP.attlocal.net>
2017-02-11 18:10:14 -0800

    before adding cancancan

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170205035822) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.string   "link"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "slug"
