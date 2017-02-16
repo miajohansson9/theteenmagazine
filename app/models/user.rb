8c0aeecd0841a794da3045b62835c23dc5650fb1
Mia Johansson <miajohansson@Mias-MBP.attlocal.net>
2017-02-15 16:16:18 -0800

    not registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 end
