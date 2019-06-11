f098784db10380233f966a6656ce94f5ec611c92
miajohansson <johansson.mia09@gmail.com>
2019-06-10 17:22:07 -0700

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  devise_scope :user do
-    # get "/sign_in" => "users/sessions#new" # custom path to login/sign_in
-    get "/apply" => "users/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-  end
   devise_for :users, controllers: { :registrations => "users/registrations", :confirmations => "users/confirmations" }
 
   resources :users
@@ -22,7 +18,8 @@ Rails.application.routes.draw do
   get 'how-to-style-your-articles' => 'pages#styling'
   get 'ranking' => 'pages#ranking'
   get 'privacy-policy' => 'pages#privacy'
-  get 'submitted' => 'pages#submitted'
+  get "/login" => "users/sessions#new" # custom path to login/sign_in
+  get "/apply" => "applies#new"
 
   resources :posts, only: [:new, :create]
   resources :posts, path: "", except: [:new, :create]
