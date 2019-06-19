9374a45cfe4dfa542e8752cc9d47a6130d37f258
miajohansson <johansson.mia09@gmail.com>
2019-06-19 13:53:51 -0700

    better ranking system

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
 
   def index
     @posts = Post.all.order("created_at desc")
+    @postsranking = Post.all.order("post_impressions desc")
     @posts_featured = Post.approved.all.order("created_at desc").first
     @posts_approved = Post.approved.all.order("created_at desc")
     @posts_approved_academics = Post.academics.all.order("created_at desc")
@@ -11,16 +12,6 @@ class WelcomeController < ApplicationController
     @category_fashion = Category.first
     @category_beauty = Category.second
     @category_entertainment = Category.third
-    @first_post = Post.first_rank.all
-    @second_post = Post.second_rank.all
-    @third_post = Post.third_rank.all
-    @fourth_post = Post.fourth_rank.all
-    @fifth_post = Post.fifth_rank.all
-    @sixth_post = Post.sixth_rank.all
-    @seventh_post = Post.seventh_rank.all
-    @eighth_post = Post.eighth_rank.all
-    @nineth_post = Post.nineth_rank.all
-    @tenth_post = Post.tenth_rank.all
   end
 
   def show
