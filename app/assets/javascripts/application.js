24277a6d9eb60c4754e97e222cefa93766d1c591
Mia Johansson <miajohansson@mias-mbp.attlocal.net>
2020-05-11 19:29:30 -0700

    css fixes and updated pretty slide-out nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,6 +24,31 @@ $('document').ready(function() {
   }, 2500);
 });
 
+/*====================================
+=            ON DOM READY            =
+====================================*/
+$(function() {
+    // Toggle Nav on Click
+    $('.toggle-nav').click(function() {
+        // Calling a function in case you want to expand upon this.
+        toggleNav();
+    });
+});
+
+/*========================================
+=            CUSTOM FUNCTIONS            =
+========================================*/
+function toggleNav() {
+    if ($('#site-wrapper').hasClass('show-nav')) {
+        // Do things on Nav Close
+        $('#site-wrapper').removeClass('show-nav');
+    } else {
+        // Do things on Nav Open
+        $('#site-wrapper').addClass('show-nav');
+    }
+    //$('#site-wrapper').toggleClass('show-nav');
+}
+
 function animateValue(id, start, end, duration) {
     // assumes integer values for start and end
 
