fdbea1779e8bf638e6130d67fc01bd33ccd2c243
Mia Johansson <miajohansson@mias-mbp.attlocal.net>
2020-05-31 14:19:13 -0700

    made homepage ads prettier

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 
   def index
     @posts_approved = Post.published.limit(40).order("publish_at desc")
-    @postsranking = Post.published.where(:publish_at => (Time.now - 3.months)..Time.now).order("post_impressions desc").limit(10)
+    @postsranking = Post.published.where(:publish_at => (Time.now - 3.months)..Time.now).order("post_impressions desc").limit(8)
   end
 
   def show
