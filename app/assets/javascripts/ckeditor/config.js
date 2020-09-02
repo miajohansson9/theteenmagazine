2263018cc73071f30d42e6ab7a522dcf73f1b3cb
Mia Johansson <miajohansson@Mias-MacBook-Pro.local>
2020-09-02 12:36:32 -0400

    script tags allowed in ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -15,11 +15,22 @@ CKEDITOR.editorConfig = function (config) {
 		{ name: 'about', groups: [ 'about' ] }
 	];
 
+  config.allowedContent = {
+        script: true,
+        $1: {
+            // This will set the default set of elements
+            elements: CKEDITOR.dtd,
+            attributes: true,
+            styles: true,
+            classes: true
+        }
+    };
+
 	config.removeButtons = 'Save,NewPage,Preview,Print,Templates,Cut,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,CopyFormatting,Outdent,Indent,CreateDiv,JustifyBlock,JustifyRight,BidiLtr,BidiRtl,Language,Anchor,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,Font,FontSize,ShowBlocks,About,Scayt,Copy,Maximize,RemoveFormat,PasteFromWord,PasteText,Paste,Find,Undo,Redo';
   config.disableNativeSpellChecker = false;
 
   config.removePlugins = 'scayt,wsc';
   config.filebrowserImageBrowseUrl = '/ckeditor/pictures';
   config.filebrowserImageUploadUrl = '/ckeditor/pictures';
-  config.pasteFilter = 'p; a[!href]';
+  config.pasteFilter = 'p; a[!href];';
 }
