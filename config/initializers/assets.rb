f2e00810d7abf68166cb51655a27ccb0b3d0703d
Mia Johansson <miajohansson@Mias-MacBook-Pro.local>
2020-11-08 13:35:12 -0500

    moved assets precompile to separate folder

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( onboarding.css )
 Rails.application.config.assets.precompile += %w[ckeditor/config.js]
+Rails.application.config.assets.precompile += ['*.js', '/plugins/*.js', '*.css', '*.scss', 'mdbootstrap/css/*.css']
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
