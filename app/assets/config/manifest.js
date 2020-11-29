77f0af1674d0d7c4a3a2a3f219e039a4e1811265
Mia <miajohansson@college.harvard.edu>
2020-11-29 18:33:52 -0500

    do not check precompiled assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
-//= link_directory ../javascripts/plugins .js
 //= link_directory ../stylesheets .css
-//= link mdbootstrap/css/bootstrap.min.css
-//= link mdbootstrap/css/mdb.min.css
--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -2,7 +2,7 @@ class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
                     url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
                     path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
-                    styles: { content: '600>', thumb: '118x100#' }
+                    styles: { content: '500>', thumb: '118x100#' }
 
   validates_attachment_presence :data
   validates_attachment_size :data, less_than: 50.megabytes
