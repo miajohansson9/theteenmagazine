ba47784151bc3d3ff05b6672e6eca6a4f53b9c85
Mia <miajohansson@college.harvard.edu>
2020-11-30 18:04:42 -0500

    css and js fix

--- app/assets/javascripts/plugins.js
+++ app/assets/javascripts/plugins.js
@@ -1,1 +1,2 @@
+//= require mdb.min
 //= require_tree ./plugins
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,9 +26,11 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = :sass
   config.assets.check_precompiled_asset = false
-  
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+
+  config.public_file_server.enabled = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
