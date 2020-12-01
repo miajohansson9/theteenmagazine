d4d30e8d4186545fc475f2f7ddf9c70c16568165
Mia <miajohansson@college.harvard.edu>
2020-11-30 20:42:49 -0500

    temporarily compiling assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.assets.check_precompiled_asset = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.public_file_server.enabled = true
 
