4ff8ba33bb7adaac3eb35f44698b89dcd552ef0e
Mia <miajohansson@college.harvard.edu>
2020-12-01 00:10:50 -0500

    updated ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,3 +1,27 @@
 CKEDITOR.editorConfig = function (config) {
+  config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
 
+	config.removeButtons = 'Save,NewPage,Preview,Print,Templates,Cut,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,CopyFormatting,Outdent,Indent,CreateDiv,JustifyBlock,JustifyRight,BidiLtr,BidiRtl,Language,Anchor,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,Font,FontSize,ShowBlocks,About,Scayt,Copy,Maximize,PasteFromWord,PasteText,Paste,Find,Undo,Redo';
+  config.disableNativeSpellChecker = false;
+
+  config.removePlugins = 'scayt,wsc';
+  config.filebrowserImageBrowseUrl = '/ckeditor/pictures';
+  config.filebrowserImageUploadUrl = '/ckeditor/pictures';
+  config.pasteFilter = 'p; a[!href];';
 }
+
+CKEDITOR.config.allowedContent = true;
