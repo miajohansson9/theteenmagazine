bc7e775c996dd9f756aedb1fed3b1458108dd926
Mia <miajohansson@college.harvard.edu>
2020-12-04 13:40:06 -0500

    fixed manifest file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
+//= link_directory ../javascripts/mdbootstrap .js
 //= link_directory ../javascripts/plugins .js
 //= link_directory ../javascripts/ckeditor
 //= link_directory ../javascripts/ckeditor/lang en.js
