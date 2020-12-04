8b87686790b36fb4fb8c2ab93d31d7e2e5e2829d
Mia <miajohansson@college.harvard.edu>
2020-12-04 13:32:53 -0500

    only required js on pages that need it

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( ckeditor/* plugins/* writer.js)
+Rails.application.config.assets.precompile += %w( *.js ckeditor/* plugins/* writer.js)
