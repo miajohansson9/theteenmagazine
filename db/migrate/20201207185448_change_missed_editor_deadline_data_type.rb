426f16b540b660e50b0bc801bf5ad6106619fd51
Mia <miajohansson@college.harvard.edu>
2020-12-07 15:04:07 -0500

    udpated rails migrate column cast datatype

--- db/migrate/20201207185448_change_missed_editor_deadline_data_type.rb
+++ db/migrate/20201207185448_change_missed_editor_deadline_data_type.rb
@@ -1,5 +1,5 @@
 class ChangeMissedEditorDeadlineDataType < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :missed_editor_deadline, :datetime
+    change_column :users, :missed_editor_deadline, 'timestamp USING CAST(missed_editor_deadline AS timestamp)'
   end
 end
