a856999289794b7c4d6f1af65912627a53625cbe
Mia <miajohansson@college.harvard.edu>
2021-01-14 15:19:36 -0500

    fixed manifest file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/users .css
 //= link_directory ../javascripts .js
 //= link_directory ../javascripts/mdbootstrap .js
 //= link_directory ../javascripts/plugins .png
