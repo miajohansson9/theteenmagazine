c107ebb890afc52b1a5c592545a282870032d151
Mia <miajohansson@college.harvard.edu>
2021-01-16 13:05:25 -0500

    fixed js

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  setTimeout(function() {
+    $('.notice').slideUp();
+  }, 1500);
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -18,13 +21,6 @@ $(document).ready(function() {
     bounds.bottom = bounds.top + this.outerHeight();
     return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
   };
-  $.fn.triggerCheck = function(){
-    if ($('.checkmark').isOnScreen()) {
-      $('.checkmark').addClass("checkmark");
-      $('.checkmark path').addClass("checkmark__check");
-      $('.checkmark circle').addClass("checkmark__circle");
-    }
-  };
   $("#submit-comment-modal-button").click(function() {
     $("#submit-comment-modal-button").addClass("disabled");
     $(".spinner-button").removeClass("hide");
@@ -32,9 +28,13 @@ $(document).ready(function() {
   $('a[href^="https://"]').not('a[href*=theteenmagazine]').attr('target','_blank');
   $('a[href^="http://"]').not('a[href*=theteenmagazine]').attr('target','_blank');
   $('[data-toggle="tooltip"]').tooltip();
-  setTimeout(function() {
-    $('.notice').slideUp();
-  }, 1500);
+  $.fn.triggerCheck = function(){
+    if ($('.checkmark').isOnScreen()) {
+      $('.checkmark').addClass("checkmark");
+      $('.checkmark path').addClass("checkmark__check");
+      $('.checkmark circle').addClass("checkmark__circle");
+    }
+  };
 });
 $(window).on('scroll',function(){
   if ($('.header-navigation').isOnScreen()) {
