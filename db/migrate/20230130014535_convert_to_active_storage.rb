9ad4a295c1b86e8233365a465330c04d66b87e05
Mia <24357628+miajohansson9@users.noreply.github.com>
2023-02-02 10:48:47 -0500

    updated migration

--- db/migrate/20230130014535_convert_to_active_storage.rb
+++ db/migrate/20230130014535_convert_to_active_storage.rb
@@ -6,26 +6,25 @@ class ConvertToActiveStorage < ActiveRecord::Migration[5.2]
   def up
     # postgres
     get_blob_id = 'LASTVAL()'
-    # mysql / mariadb
-    # get_blob_id = 'LAST_INSERT_ID()'
-    # sqlite
-    # get_blob_id = 'LAST_INSERT_ROWID()'
+    connection = ActiveRecord::Base.connection.raw_connection
 
-    active_storage_blob_statement = ActiveRecord::Base.connection.raw_connection.prepare(<<-SQL)
+    active_storage_blob_statement = connection.prepare('active_storage_blob_statement', <<-SQL)
       INSERT INTO active_storage_blobs (
-        key, filename, content_type, metadata, byte_size, checksum, created_at
-      ) VALUES (?, ?, ?, '{}', ?, ?, ?)
+        "key", filename, content_type, metadata, byte_size, checksum, created_at
+      ) VALUES ($1, $2, $3, '{}', $4, $5, $6)
     SQL
 
-    active_storage_attachment_statement = ActiveRecord::Base.connection.raw_connection.prepare(<<-SQL)
+    active_storage_attachment_statement = connection.prepare('active_storage_attachment_statement', <<-SQL)
       INSERT INTO active_storage_attachments (
         name, record_type, record_id, blob_id, created_at
-      ) VALUES (?, ?, ?, #{get_blob_id}, ?)
+      ) VALUES ($1, $2, $3, #{get_blob_id}, $4)
     SQL
 
+#    models = [ Payment, User, etc]
+    Rails.application.eager_load!
     models = ActiveRecord::Base.descendants.reject(&:abstract_class?)
 
-    transaction do
+    ActiveRecord::Base.transaction do
       models.each do |model|
         attachments = model.column_names.map do |c|
           if c =~ /(.+)_file_name$/
@@ -33,33 +32,95 @@ class ConvertToActiveStorage < ActiveRecord::Migration[5.2]
           end
         end.compact
 
+        next if attachments.blank?
+
         model.find_each.each do |instance|
           attachments.each do |attachment|
-            begin
-              if instance.send(attachment).exists?
-                active_storage_blob_statement.execute(
-                  key(instance, attachment),
-                  instance.send("#{attachment}_file_name"),
-                  instance.send("#{attachment}_content_type"),
-                  instance.send("#{attachment}_file_size"),
-                  checksum(instance.send(attachment)),
-                  instance.updated_at.iso8601
-                )
+            next if instance.send(attachment).path.blank?
 
-                active_storage_attachment_statement.
-                  execute(attachment, model.name, instance.id, instance.updated_at.iso8601)
-              end
-            rescue
-              puts "#{instance.send(attachment)} did not get converted"
-            end
+            connection.exec_prepared(
+              'active_storage_blob_statement',
+              [
+                key(instance, attachment),
+                instance.send("#{attachment}_file_name"),
+                instance.send("#{attachment}_content_type"),
+                instance.send("#{attachment}_file_size"),
+                checksum(instance.send(attachment)),
+                instance.updated_at.iso8601
+              ]
+            )
+
+            connection.exec_prepared(
+              'active_storage_attachment_statement',
+              [
+                attachment, 
+                model.name, 
+                instance.id, 
+                instance.updated_at.iso8601
+              ]
+            )
           end
         end
       end
     end
-
-    active_storage_attachment_statement.close
-    active_storage_blob_statement.close
   end
+
+  #   # postgres
+  #   get_blob_id = 'LASTVAL()'
+  #   # mysql / mariadb
+  #   # get_blob_id = 'LAST_INSERT_ID()'
+  #   # sqlite
+  #   # get_blob_id = 'LAST_INSERT_ROWID()'
+
+  #   active_storage_blob_statement = ActiveRecord::Base.connection.raw_connection.prepare(<<-SQL)
+  #     INSERT INTO active_storage_blobs (
+  #       key, filename, content_type, metadata, byte_size, checksum, created_at
+  #     ) VALUES (?, ?, ?, '{}', ?, ?, ?)
+  #   SQL
+
+  #   active_storage_attachment_statement = ActiveRecord::Base.connection.raw_connection.prepare(<<-SQL)
+  #     INSERT INTO active_storage_attachments (
+  #       name, record_type, record_id, blob_id, created_at
+  #     ) VALUES (?, ?, ?, #{get_blob_id}, ?)
+  #   SQL
+
+  #   models = ActiveRecord::Base.descendants.reject(&:abstract_class?)
+
+  #   transaction do
+  #     models.each do |model|
+  #       attachments = model.column_names.map do |c|
+  #         if c =~ /(.+)_file_name$/
+  #           $1
+  #         end
+  #       end.compact
+
+  #       model.find_each.each do |instance|
+  #         attachments.each do |attachment|
+  #           begin
+  #             if instance.send(attachment).exists?
+  #               active_storage_blob_statement.execute(
+  #                 key(instance, attachment),
+  #                 instance.send("#{attachment}_file_name"),
+  #                 instance.send("#{attachment}_content_type"),
+  #                 instance.send("#{attachment}_file_size"),
+  #                 checksum(instance.send(attachment)),
+  #                 instance.updated_at.iso8601
+  #               )
+
+  #               active_storage_attachment_statement.
+  #                 execute(attachment, model.name, instance.id, instance.updated_at.iso8601)
+  #             end
+  #           rescue
+  #             puts "#{instance.send(attachment)} did not get converted"
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+
+  #   active_storage_attachment_statement.close
+  #   active_storage_blob_statement.close
+  # end
 
   def down
     raise ActiveRecord::IrreversibleMigration
@@ -84,9 +145,13 @@ class ConvertToActiveStorage < ActiveRecord::Migration[5.2]
     #Digest::MD5.base64digest(File.read(url))
 
     # remote files stored on another person's computer:
-    url = "https:#{attachment.url}"
-    puts url
-    Digest::MD5.base64digest(Net::HTTP.get(URI(url)))
+    begin
+      url = "https:#{attachment.url}"
+      puts url
+      Digest::MD5.base64digest(Net::HTTP.get(URI(url)))
+    rescue StandardError
+      "fakechecksum"
+    end
   end
 end
 
