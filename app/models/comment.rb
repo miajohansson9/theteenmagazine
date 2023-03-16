cd8ee567d7da6858db85f37568139eeaf79edc9d
Mia <24357628+miajohansson9@users.noreply.github.com>
2023-03-16 16:13:33 -0700

    show views and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user, optional: true
   has_many :comments
+
+  scope :published,
+        -> {
+          where(public: true)
+        }
 end
