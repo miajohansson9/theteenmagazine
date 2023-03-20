300f2febc2ba3ea72ed6d836930584164043cc56
Mia <24357628+miajohansson9@users.noreply.github.com>
2023-03-20 11:53:44 -0700

    fixed reset button and change password page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/users .css
+//= link_directory ../stylesheets/devise .css
 //= link_directory ../javascripts .js
 //= link_directory ../javascripts/mdbootstrap .js
 //= link_directory ../javascripts/plugins .js
