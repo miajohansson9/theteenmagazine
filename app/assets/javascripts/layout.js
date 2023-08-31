6e8b723d268da932657a5e1346bd93b1055651d2
Mia Johansson <24357628+miajohansson9@users.noreply.github.com>
2023-08-30 19:42:18 -0700

    fixed on javascript disabled too

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $(".notice").slideUp();
   }, 1500);
 
+  $("#content").removeClass('javascript-disabled');
   // hide images where image policy hasn't been agreed to
   $('.image-policy-not-agreed-to').each(function() {
     // Find and replace the src attribute content with an empty string
